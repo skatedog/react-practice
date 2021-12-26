7ab90ab5c5076726c1df7c3dcef7bbdff3f4f9d1
skatedog <skatedog0001@gmail.com>
2021-12-26 17:00:55 +0900

    [Practice]CssModules

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,27 +1,6 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { ColoredMessage } from "./components/ColoredMessage";
-
+import { CssModules } from "./components/CssModules";
 export const App = () => {
-
-
-  const [num, setNum] = useState(0);
-
-  useEffect(() => {
-    alert();
-  }, [num]);
-
-  const onClickButton = () => {
-    setNum((prev) => prev + 1);
-  }
-
   return (
-    <>
-      <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
-      <ColoredMessage color="pink">元気です！</ColoredMessage>
-      <button onClick={onClickButton}>ボタン</button>
-      <p>{num}</p>
-    </>
+    <CssModules />
   );
 };
