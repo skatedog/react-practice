228803944df1b170795a4051fde968b9f1eb342a
skatedog <skatedog0001@gmail.com>
2021-12-26 20:20:23 +0900

    [Practice]コンポーネントのメモ化

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,21 @@
-import { Emotion } from "./components/Emotion";
-export const App = () => {
+import { useState, memo } from "react";
+import { Child1 } from "./components/Child1";
+import { Child4 } from "./components/Child4";
+
+export const App = memo(() => {
+  console.log("Appレンダリング");
+  const [num, setNum] = useState(0);
+
+  const onClickButton = () => {
+    setNum(num + 1);
+  };
+
   return (
-    <Emotion />
+    <>
+      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
+      <Child1 />
+      <Child4 />
+    </>
   );
-};
+});
