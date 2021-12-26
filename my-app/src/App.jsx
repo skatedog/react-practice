4f86a8f0ba0c3d8a75b7e776b5215c4dd94ca767
skatedog <skatedog0001@gmail.com>
2021-12-26 14:52:55 +0900

    [Practice]Props

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,17 @@
+import { ColoredMessage } from "./components/ColoredMessage";
+
 export const App = () => {
+
+  const onClickButton = () => {
+    alert("hoge");
+  }
+
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColoredMessage color="blue" message="お元気ですか?" />
+      <ColoredMessage color="pink" message="元気です！" />
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
