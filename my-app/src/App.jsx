4afd6b7cce196ceae951919db655f95e02231727
skatedog <skatedog0001@gmail.com>
2021-12-27 10:35:09 +0900

    [Practice]グローバルstate

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,25 +1,17 @@
-import { useState, memo, useCallback } from "react";
-import { Child1 } from "./components/Child1";
-import { Child4 } from "./components/Child4";
+import { useContext } from "react";
+import { Card } from "./components/Card";
+import { AdminFlagContext } from "./components/providers/AdminFlagProvider";
 
-export const App = memo(() => {
-  console.log("Appレンダリング");
-  const [num, setNum] = useState(0);
+export const App = () => {
+  const { isAdmin, setIsAdmin } = useContext(AdminFlagContext);
 
-  const onClickButton = () => {
-    setNum(num + 1);
-  };
-
-  const onClickReset = useCallback(() => {
-    setNum(0);
-  }, []);
+  const onClickSwitch = () => setIsAdmin(!isAdmin);
 
   return (
-    <>
-      <button onClick={onClickButton}>ボタン</button>
-      <p>{num}</p>
-      <Child1 onClickReset={onClickReset} />
-      <Child4 />
-    </>
+    <div>
+      {isAdmin ? <span>管理者</span> : <span>管理者以外</span>}
+      <button onClick={onClickSwitch}>切り替え</button>
+      <Card isAdmin={isAdmin} />
+    </div>
   );
-});
+};
